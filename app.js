e39e3a5fa7ee5e63f12609eff683282ba18e344c
mszpila <szpila.mikolaj@gmail.com>
2020-11-14 21:38:21 +0100

    Update

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 // app.use(express.static("/files"));
-// app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/public", express.static("public"));
 app.use(express.static(path.join(__dirname, "view/build")));
